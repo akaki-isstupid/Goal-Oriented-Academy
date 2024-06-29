1e3c4cbff19ab1630829241836271bbec4a0bf97
akaki-isstupid <akaki7618@gmail.com>
2024-06-29 00:15:52 -0700

    commit

--- Level 07/Homework/task01.py
+++ Level 07/Homework/task01.py
@@ -1,8 +1,8 @@
 # input numbers
-num1 = 16
-num2 = 45
-num3 = 7
-num4 = 4
+num1 = int(input("enter any number:"))
+num2 = int(input("enter any number:"))
+num3 = int(input("enter any number:"3))
+num4 = int(input("enter any number:"))
 print(num1 + num2 + num3 + num4)
 print(num1 - num2 - num3 - num4)
 print(num1 * num2 * num3 * num4)
